791a2b982f082d84a8ef76a0c945b2b9c5af9a63
Simon Coutu <coutu.simon@gmail.com>
2016-12-28 13:17:13 -0500

    Test branche

---
+++ GestionVentes/GestionVentes/Models/VendeurViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace GestionVentes.Models
+{
+    public class VendeurViewModel
+    {
+        [Required]
+        [Display(Name = "Identifiant")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Nom du vendeur")]
+        public string Nom { get; set; }
+    }
+
+}
